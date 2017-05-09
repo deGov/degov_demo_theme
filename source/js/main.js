dba11b6995b30a5484a2ed417bb7d9d50cb3d1bc
Martin Spencer <m.spencer@1xinternet.de>
2017-05-09 13:06:07 +0200

    init commit for the demo theme

---
+++ source/js/main.js
@@ -0,0 +1,13 @@
+(function ($, Drupal) {
+  'use strict';
+
+  /*
+  This is what a behavior should look like:
+  Drupal.behaviors.custom_behavior = {
+    attach: context => {
+      $(context).find('.le-selector').once('custom_behavior').each(function () {
+        // ... le code
+      });
+    }
+  };*/
+})(jQuery, window.Drupal);
